4b88ea023d0abe50cf01cfecbf9ff04fb1ad9fc4
na1-4an <parkne0114@gmail.com>
2025-08-29 18:35:06 +0900

    Update

--- run_bal.sh
+++ run_bal.sh
@@ -1,6 +1,1 @@
-CUDA_VISIBLE_DEVICES=4 python main.py --dataset Cora --alpha 0.25 --beta 0.25 --model_type BalGNN --num_layers 8
-CUDA_VISIBLE_DEVICES=4 python main.py --dataset CiteSeer --alpha 0.25 --beta 0.75 --model_type BalGNN --num_layers 8 
-CUDA_VISIBLE_DEVICES=4 python main.py --dataset texas --alpha 0.75 --beta 0.25 --model_type BalGNN --num_layers 8 
-CUDA_VISIBLE_DEVICES=4 python main.py --dataset cornell --alpha 0.25 --beta 0.25 --model_type BalGNN --num_layers 8 
-CUDA_VISIBLE_DEVICES=4 python main.py --dataset wisconsin --alpha 0.5 --beta 1 --model_type BalGNN --num_layers 8
-CUDA_VISIBLE_DEVICES=4 python main.py --dataset chameleon --alpha 0.5 --beta 1 --model_type BalGNN --num_layers 8 
+python main.py --dataset Cora --alpha 0.25 --beta 0.25 --model_type BalGNN --num_layers 8
